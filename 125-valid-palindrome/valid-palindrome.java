64ff20e711cb959cdb4f7ad9467fdb741d519d8b
prachichaudhari19 <prachichaudhari82@gmail.com>
2026-04-05 12:08:06 +0530

    Time: 2 ms (99.42%) | Memory: 44.4 MB (61.08%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,25 +1,23 @@
-class Solution {
-    public boolean isPalindrome(String s) {
-        if (s.isEmpty()) {
-        	return true;
+class Solution{
+    public static boolean isPalindrome(String s){
+        int left = 0;
+        int right = s.length()-1;
+        while(left<right){
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+             while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+               return false; 
+            } 
+            left++;
+        right--;
+
         }
-        int start = 0;
-        int last = s.length() - 1;
-        while(start <= last) {
-        	char currFirst = s.charAt(start);
-        	char currLast = s.charAt(last);
-        	if (!Character.isLetterOrDigit(currFirst )) {
-        		start++;
-        	} else if(!Character.isLetterOrDigit(currLast)) {
-        		last--;
-        	} else {
-        		if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
-        			return false;
-        		}
-        		start++;
-        		last--;
-        	}
-        }
-        return true;
+            return true;
     }
+
+
 }
